ef058d18809449e6636f909cf8947922f5fe759a
Justin Privitera <privitera1@llnl.gov>
2025-01-27 15:15:45 -0800

    another day another test

--- src/tests/conduit/t_conduit_execution_array.cpp
+++ src/tests/conduit/t_conduit_execution_array.cpp
@@ -146,164 +146,174 @@ TEST(conduit_array, array_stride_int8)
 
 }
 
-// //-----------------------------------------------------------------------------
-// TEST(conduit_array, array_stride_int8_external)
-// {
-//     std::vector<int64> data(20,0);
+//-----------------------------------------------------------------------------
+TEST(conduit_array, array_stride_int8_external)
+{
+    std::vector<int64> data(20,0);
 
-//     for(int i=0;i<20;i+=2)
-//     {
-//         data[i] = i/2;
-//     }
+    for(int i=0;i<20;i+=2)
+    {
+        data[i] = i/2;
+    }
 
-//     for(int i=1;i<20;i+=2)
-//     {
-//         data[i] = -i/2;
-//     }
-//     std::cout << "Full Data" << std::endl;
+    for(int i=1;i<20;i+=2)
+    {
+        data[i] = -i/2;
+    }
+    std::cout << "Full Data" << std::endl;
 
-//     for(int i=0;i<20;i++)
-//     {
-//         std::cout << (int64) data[i] << " ";
-//     }
-//     std::cout << std::endl;
+    for(int i=0;i<20;i++)
+    {
+        std::cout << (int64) data[i] << " ";
+    }
+    std::cout << std::endl;
 
-//     Node n;
-//     n["value"].set_external(data);
+    Node n;
+    n["value"].set_external(data);
 
-//     int64_array arr = n["value"].as_int64_array();
+    int64_exec_array arr(n["value"]);
 
-//     for(int i=0;i<20;i++)
-//     {
-//         // note: the cast is for proper printing to std::out
-//         std::cout << "value[" << i << "] = " << arr[i] << std::endl;
-//     }
-//     std::cout << std::endl;
+    for(int i=0;i<20;i++)
+    {
+        // note: the cast is for proper printing to std::out
+        std::cout << "value[" << i << "] = " << arr[i] << std::endl;
+    }
+    std::cout << std::endl;
 
-//     data[2]*=10;
-//     data[3]*=10;
+    data[2]*=10;
+    data[3]*=10;
 
-//     EXPECT_EQ(arr[2],10);
-//     EXPECT_EQ(arr[3],-10);
+    EXPECT_EQ(arr[2],10);
+    EXPECT_EQ(arr[3],-10);
 
-// }
-
-
-// //-----------------------------------------------------------------------------
-// TEST(conduit_array, set_using_ptrs)
-// {
-//     //in this case we are  using std vectors to init data conveniently
-//     // we are actually testing the pointer set cases
-//     // we test std vector set cases directly in "set_using_std_vectors"
-//     std::vector<int8>  v_int8(10,-8);
-//     std::vector<int16> v_int16(10,-16);
-//     std::vector<int32> v_int32(10,-32);
-//     std::vector<int64> v_int64(10,-64);
-
-//     std::vector<uint8>  v_uint8(10,8);
-//     std::vector<uint16> v_uint16(10,16);
-//     std::vector<uint32> v_uint32(10,32);
-//     std::vector<uint64> v_uint64(10,64);
-
-//     std::vector<float32> v_float32(10,32.0);
-//     std::vector<float64> v_float64(10,64.0);
+}
 
 
+//-----------------------------------------------------------------------------
+TEST(conduit_array, set_using_ptrs)
+{
+    //in this case we are  using std vectors to init data conveniently
+    // we are actually testing the pointer set cases
+    // we test std vector set cases directly in "set_using_std_vectors"
+    std::vector<int8>  v_int8(10,-8);
+    std::vector<int16> v_int16(10,-16);
+    std::vector<int32> v_int32(10,-32);
+    std::vector<int64> v_int64(10,-64);
 
-//     Node n;
+    std::vector<uint8>  v_uint8(10,8);
+    std::vector<uint16> v_uint16(10,16);
+    std::vector<uint32> v_uint32(10,32);
+    std::vector<uint64> v_uint64(10,64);
 
-//     // int8_array
-//     n["vint8"].set(DataType::int8(10));
-//     n["vint8"].as_int8_array().set(&v_int8[0],10);
-//     int8 *n_int8_ptr = n["vint8"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_int8_ptr[i],v_int8[i]);
-//     }
-
-//     // int16_array
-//     n["vint16"].set(DataType::int16(10));
-//     n["vint16"].as_int16_array().set(&v_int16[0],10);
-//     int16 *n_int16_ptr = n["vint16"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_int16_ptr[i],v_int16[i]);
-//     }
-
-//     // int32_array
-//     n["vint32"].set(DataType::int32(10));
-//     n["vint32"].as_int32_array().set(&v_int32[0],10);
-//     int32 *n_int32_ptr = n["vint32"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_int32_ptr[i],v_int32[i]);
-//     }
-
-//     // int64_array
-//     n["vint64"].set(DataType::int64(10));
-//     n["vint64"].as_int64_array().set(&v_int64[0],10);
-//     int64 *n_int64_ptr = n["vint64"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_int64_ptr[i],v_int64[i]);
-//     }
-
-//     // uint8_array
-//     n["vuint8"].set(DataType::uint8(10));
-//     n["vuint8"].as_uint8_array().set(&v_uint8[0],10);
-//     uint8 *n_uint8_ptr = n["vuint8"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_uint8_ptr[i],v_uint8[i]);
-//     }
-
-//     // uint16_array
-//     n["vuint16"].set(DataType::uint16(10));
-//     n["vuint16"].as_uint16_array().set(&v_uint16[0],10);
-//     uint16 *n_uint16_ptr = n["vuint16"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_uint16_ptr[i],v_uint16[i]);
-//     }
-
-//     // uint32_array
-//     n["vuint32"].set(DataType::uint32(10));
-//     n["vuint32"].as_uint32_array().set(&v_uint32[0],10);
-//     uint32 *n_uint32_ptr = n["vuint32"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_uint32_ptr[i],v_uint32[i]);
-//     }
-
-//     // uint64_array
-//     n["vuint64"].set(DataType::uint64(10));
-//     n["vuint64"].as_uint64_array().set(&v_uint64[0],10);
-//     uint64 *n_uint64_ptr = n["vuint64"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_uint64_ptr[i],v_uint64[i]);
-//     }
+    std::vector<float32> v_float32(10,32.0);
+    std::vector<float64> v_float64(10,64.0);
 
 
-//     // float32_array
-//     n["vfloat32"].set(DataType::float32(10));
-//     n["vfloat32"].as_float32_array().set(&v_float32[0],10);
-//     float32 *n_float32_ptr = n["vfloat32"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_float32_ptr[i],v_float32[i]);
-//     }
 
-//     // float64_array
-//     n["vfloat64"].set(DataType::float64(10));
-//     n["vfloat64"].as_float64_array().set(&v_float64[0],10);
-//     float64 *n_float64_ptr = n["vfloat64"].value();
-//     for(size_t i=0;i<10;i++)
-//     {
-//         EXPECT_EQ(n_float64_ptr[i],v_float64[i]);
-//     }
+    Node n;
 
-// }
+    // int8_array
+    n["vint8"].set(DataType::int8(10));
+    int8_exec_array arr_int8(n["vint8"]);
+    arr_int8.set(&v_int8[0],10);
+    int8 *n_int8_ptr = n["vint8"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_int8_ptr[i],v_int8[i]);
+    }
+
+    // int16_array
+    n["vint16"].set(DataType::int16(10));
+    int16_exec_array arr_int16(n["vint16"]);
+    arr_int16.set(&v_int16[0],10);
+    int16 *n_int16_ptr = n["vint16"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_int16_ptr[i],v_int16[i]);
+    }
+
+    // int32_array
+    n["vint32"].set(DataType::int32(10));
+    int32_exec_array arr_int32(n["vint32"]);
+    arr_int32.set(&v_int32[0],10);
+    int32 *n_int32_ptr = n["vint32"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_int32_ptr[i],v_int32[i]);
+    }
+
+    // int64_array
+    n["vint64"].set(DataType::int64(10));
+    int64_exec_array arr_int64(n["vint64"]);
+    arr_int64.set(&v_int64[0],10);
+    int64 *n_int64_ptr = n["vint64"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_int64_ptr[i],v_int64[i]);
+    }
+
+    // uint8_array
+    n["vuint8"].set(DataType::uint8(10));
+    uint8_exec_array arr_uint8(n["vuint8"]);
+    arr_uint8.set(&v_uint8[0],10);
+    uint8 *n_uint8_ptr = n["vuint8"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_uint8_ptr[i],v_uint8[i]);
+    }
+
+    // uint16_array
+    n["vuint16"].set(DataType::uint16(10));
+    uint16_exec_array arr_uint16(n["vuint16"]);
+    arr_uint16.set(&v_uint16[0],10);
+    uint16 *n_uint16_ptr = n["vuint16"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_uint16_ptr[i],v_uint16[i]);
+    }
+
+    // uint32_array
+    n["vuint32"].set(DataType::uint32(10));
+    uint32_exec_array arr_uint32(n["vuint32"]);
+    arr_uint32.set(&v_uint32[0],10);
+    uint32 *n_uint32_ptr = n["vuint32"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_uint32_ptr[i],v_uint32[i]);
+    }
+
+    // uint64_array
+    n["vuint64"].set(DataType::uint64(10));
+    uint64_exec_array arr_uint64(n["vuint64"]);
+    arr_uint64.set(&v_uint64[0],10);
+    uint64 *n_uint64_ptr = n["vuint64"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_uint64_ptr[i],v_uint64[i]);
+    }
+
+
+    // float32_array
+    n["vfloat32"].set(DataType::float32(10));
+    float32_exec_array arr_float32(n["vfloat32"]);
+    arr_float32.set(&v_float32[0],10);
+    float32 *n_float32_ptr = n["vfloat32"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_float32_ptr[i],v_float32[i]);
+    }
+
+    // float64_array
+    n["vfloat64"].set(DataType::float64(10));
+    float64_exec_array arr_float64(n["vfloat64"]);
+    arr_float64.set(&v_float64[0],10);
+    float64 *n_float64_ptr = n["vfloat64"].value();
+    for(size_t i=0;i<10;i++)
+    {
+        EXPECT_EQ(n_float64_ptr[i],v_float64[i]);
+    }
+
+}
 
 
 // //-----------------------------------------------------------------------------
